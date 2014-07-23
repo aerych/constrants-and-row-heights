bf286b6b38d1a7a526e435bee63f580b9e3318da
aerych <aerychj@gmail.com>
2014-07-23 17:22:40 -0500

    Initial Commit

---
+++ autolayouttest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  autolayouttest
+//
+//  Created by aerych on 7/23/14.
+//  Copyright (c) 2014 WordPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
